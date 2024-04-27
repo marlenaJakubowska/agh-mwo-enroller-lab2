b026feaa9d86b288bf0c5831a51aec8eab99fd48
marlenaJakubowska <mar.jakubowska90@gmail.com>
2024-04-27 09:43:29 +0200

    Add filtering to getAll participants

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -17,37 +17,45 @@ public class ParticipantRestController {
 	@Autowired
 	ParticipantService participantService;
 
-	@RequestMapping(value = "", method = RequestMethod.GET)
-	public ResponseEntity<?> getParticipants() {
-		Collection<Participant> participants = participantService.getAll();
-		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
+	@GetMapping(value = "")
+	public ResponseEntity<?> getParticipants(
+			@RequestParam(required = false) String sortBy,
+			@RequestParam(required = false, defaultValue = "ASC") String sortOrder,
+			@RequestParam(required = false) String key) {
+
+		if (!sortOrder.equalsIgnoreCase("ASC") && !sortOrder.equalsIgnoreCase("DESC")) {
+			sortOrder = "ASC";
+		}
+
+		Collection<Participant> participants = participantService.getAll(sortOrder, sortBy, key);
+		return new ResponseEntity<>(participants, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> getParticipant(@PathVariable("id") String login) {
 		Participant participant = participantService.findByLogin(login);
 		if (participant == null) {
-			return new ResponseEntity(HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
-		return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+		return new ResponseEntity<>(participant, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public ResponseEntity<?> addParticipant(@RequestBody Participant participant) {
 		if (participantService.findByLogin(participant.getLogin()) != null) {
-			return new ResponseEntity<String>(
-					"Unable to create. A participant with login " + participant.getLogin() + " already exist.",
-					HttpStatus.CONFLICT);
+			return new ResponseEntity<>(
+                    "Unable to create. A participant with login " + participant.getLogin() + " already exist.",
+                    HttpStatus.CONFLICT);
 		}
 		participantService.add(participant);
-		return new ResponseEntity<Participant>(participant, HttpStatus.CREATED);
+		return new ResponseEntity<>(participant, HttpStatus.CREATED);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<?> delete(@PathVariable("id") String login) {
 		Participant participant = participantService.findByLogin(login);
 		if (participant == null) {
-			return new ResponseEntity(HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		participantService.delete(participant);
 		return new ResponseEntity<Participant>(HttpStatus.OK);
@@ -57,7 +65,7 @@ public class ParticipantRestController {
 	public ResponseEntity<?> update(@PathVariable("id") String login, @RequestBody Participant updatedParticipant) {
 		Participant participant = participantService.findByLogin(login);
 		if (participant == null) {
-			return new ResponseEntity(HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		participant.setPassword(updatedParticipant.getPassword());
 		participantService.update(participant);
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -1,11 +1,14 @@
 package com.company.enroller.persistence;
 
 import com.company.enroller.model.Participant;
+import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.criteria.*;
 import java.util.Collection;
+import java.util.List;
 
 @Component("participantService")
 public class ParticipantService {
@@ -21,6 +24,32 @@ public class ParticipantService {
         Query query = connector.getSession().createQuery(hql);
         return query.list();
     }
+
+    public List<Participant> getAll(String sortOrder, String sortBy, String key) {
+        CriteriaBuilder cb = connector.getSession().getCriteriaBuilder();
+        CriteriaQuery<Participant> cq = cb.createQuery(Participant.class);
+        Root<Participant> root = cq.from(Participant.class);
+
+        if (sortBy != null && !sortBy.isEmpty()) {
+            Order order;
+            if ("ASC".equalsIgnoreCase(sortOrder)) {
+                order = cb.asc(root.get(sortBy));
+            } else if ("DESC".equalsIgnoreCase(sortOrder)) {
+                order = cb.desc(root.get(sortBy));
+            } else {
+                order = cb.asc(root.get(sortBy)); // default sort order
+            }
+            cq.orderBy(order);
+        }
+
+        if (key != null && !key.isEmpty()) {
+            Predicate filter = cb.like(root.get("login"), "%" + key + "%");
+            cq.where(filter);
+        }
+
+        return connector.getSession().createQuery(cq).getResultList();
+    }
+
 
     public Participant findByLogin(String login) {
         return connector.getSession().get(Participant.class, login);
@@ -43,6 +72,23 @@ public class ParticipantService {
         Transaction transaction = connector.getSession().beginTransaction();
         connector.getSession().delete(participant);
         transaction.commit();
+    }
+
+    public Collection<Participant> getAllFilteredSorted(String sortBy, String sortOrder, String key) {
+        try (Session session = connector.getSession()) {
+            String hql = "FROM Participant";
+            if (key != null && !key.isEmpty()) {
+                hql += " WHERE login LIKE :key";
+            }
+            if (sortBy != null && (sortBy.equalsIgnoreCase("login"))) {
+                hql += " ORDER BY login " + (sortOrder.equalsIgnoreCase("DESC") ? "DESC" : "ASC");
+            }
+            Query<Participant> query = session.createQuery(hql, Participant.class);
+            if (key != null && !key.isEmpty()) {
+                query.setParameter("key", "%" + key + "%");
+            }
+            return query.list();
+        }
     }
 
 }
